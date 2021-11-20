c090a146ddea0841c5c1efe557cbf17db68e8919
Vladimir Perveev <vladimir.perveev@branderstudio.com>
2021-11-20 17:16:50 +0200

    l

--- lib/presentation/widget/base_bottom_sheet.dart
+++ lib/presentation/widget/base_bottom_sheet.dart
@@ -10,14 +10,13 @@ class BaseBottomSheet extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return DecoratedBox(
-      decoration: const BoxDecoration(
-        color: Palette.white,
-        borderRadius: BorderRadius.only(
-          topLeft: Radius.circular(16.0),
-          topRight: Radius.circular(16.0),
+        decoration: const BoxDecoration(
+          color: Palette.white,
+          borderRadius: BorderRadius.only(
+            topLeft: Radius.circular(16.0),
+            topRight: Radius.circular(16.0),
+          ),
         ),
-      ),
-      child: child,
-    );
+        child: child);
   }
 }
